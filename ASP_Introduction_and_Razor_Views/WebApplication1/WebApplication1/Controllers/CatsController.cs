3becaf5302c6e565176ba1a03c6e9e1edf378487
unknown <pavelmitev@yahoo.com>
2021-07-04 23:44:13 +0300

    ViewData, ViewBag and ViewModel examples

--- ASP_Introduction_and_Razor_Views/WebApplication1/WebApplication1/Controllers/CatsController.cs
+++ ASP_Introduction_and_Razor_Views/WebApplication1/WebApplication1/Controllers/CatsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -7,7 +8,27 @@ namespace WebApplication1.Controllers
     {
         public IActionResult All()
         {
-            return View();
+            ////cats fetched from database.
+            //var cats = new List<string>
+            //{
+            //    "Pesho",
+            //    "Gosho"
+            //};
+
+            ////  ViewData["Cats"] = cats;
+            //// return View();
+
+            //ViewBag.Cats = cats;
+            //return View(cats);
+
+
+
+            var cats = new List<CatViewModel>
+            {
+                new CatViewModel { Name = "Pesho", Age = 12 },
+                new CatViewModel { Name = "Gosho", Age = 3 },
+            };
+            return View(cats);
         }
     }
 }
